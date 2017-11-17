9d80fff09c53407e305b78028fecdbbac0e52955
DPathirajage <dilanka@zebra.com>
2017-11-17 13:48:00 +0530

    chnaged

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello{
 
 	public static void main(String[] args){
-		System.out.println("**********hello world");
+		System.out.println("**********hello world chnaged in git test**");
 	}
 
 }
